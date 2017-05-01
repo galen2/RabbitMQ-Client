dbb395f36e9d0618e592f1201b0a32673963bfd1
ma <ma@ma-PC>
2017-05-01 22:29:48 +0800

    0501-0000

--- src/test/java/liequ/RabbitMQ_Client/AppTest.java
+++ src/test/java/liequ/RabbitMQ_Client/AppTest.java
@@ -1,5 +1,7 @@
 package liequ.RabbitMQ_Client;
 
+import j.Env;
+
 import java.util.concurrent.LinkedBlockingDeque;
 
 /**
@@ -9,14 +11,15 @@ public class AppTest {
 	
 	@org.junit.Test
 	public void queue() throws Exception{
-		LinkedBlockingDeque que = new LinkedBlockingDeque();
+		/*LinkedBlockingDeque que = new LinkedBlockingDeque();
 		que.addFirst(1);
 		que.addLast(2);
 		que.addLast(3);
 
 		
 		System.out.println(que.take());
-		System.out.println(que.take());
+		System.out.println(que.take());*/
+		System.out.println(Env.ROOT);
 
 	}
 }
